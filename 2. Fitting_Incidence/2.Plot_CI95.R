d99e3f3368aa21ba99761887285076d9202b6218
tanaphum <tana_jumbo@hotmail.com>
2026-04-28 14:56:17 +0700

    Update 2.Plot_CI95.R
    - edit plot follow the document

--- 2. Fitting_Incidence/2.Plot_CI95.R
+++ 2. Fitting_Incidence/2.Plot_CI95.R
@@ -33,6 +33,7 @@ parameters_changing <- function(parameters,pars){
   parameters$beta_r_2   <- c(pars[2], pars[2], pars[2]) * pars[3]
   parameters$beta_ar_2  <- pars[2] * pars[3]
   parameters$c_beta_r <- pars[4]
+  parameters$start_b <- 12*21
   
   return(parameters)
 }
@@ -157,69 +158,70 @@ params.rand_remove_nan <- params.rand[params.rand_complete,]
 params.rand_complete2 <- complete.cases(params.rand2)
 params.rand_remove_nan2 <- params.rand2[params.rand_complete2,]
 
-CI95 <- data.frame(getCredibleIntervals(params.rand_remove_nan,c(0.025, 0.5,0.975)))
-
+CI95 <- data.frame(getCredibleIntervals(params.rand_remove_nan, c(0.025, 0.5, 0.975)))
 times_year <- 2000:2050
+up  <- unlist(CI95[3,])
+low <- unlist(CI95[1,])
+med <- unlist(CI95[2,])
 
-up <- unlist( CI95[3,])
-low <- unlist( CI95[1,])
-plot(times_year ,unlist( CI95[2,]),type = "l",xlab= "month",ylab ="Incidence",ylim = c(0, 3e+7))
-#make polygon where coordinates start with lower limit and 
-# then upper limit in reverse order
-polygon(c(times_year,rev(times_year)),c(low,rev(up)),col = "grey75", border = FALSE)
+par(mfrow = c(1,1), mar = c(3, 3, 3, 1))  # leave right margin for legend
 
-lines(times_year ,unlist( CI95[2,]), lwd = 2,col=4)
-#add red lines on borders of polygon
-lines(times_year ,up, col="black",lty=2)
-lines(times_year ,low, col="black",lty=2)
-points(2015:2023 ,Tanzania_Incidence[, 2],col=2,type="p",pch=20,cex=2)
-title("Incidence")
-
-# Resistance Ratio
-CI95_2 <- data.frame(getCredibleIntervals(params.rand_remove_nan2,c(0.025, 0.5,0.975)))
-times_year <- 2000:2050                                                                    
-up2 <- unlist( CI95_2[3,])
-low2 <- unlist( CI95_2[1,])
-plot(times_year ,unlist( CI95_2[2,]),type = "l",xlab= "month",ylab ="Resistance Ratio",ylim = c(0, 1))
-#make polygon where coordinates start with lower limit and
-# then upper limit in reverse order
-polygon(c(times_year,rev(times_year)),c(low2,rev(up2)),col ="grey75", border = FALSE)
-lines(times_year ,unlist( CI95_2[2,]), lwd = 2,col=4)
-#add red lines on borders of polygon
-lines(times_year ,up2, col="black",lty=2)
-lines(times_year ,low2, col="black",lty=2)
-points(2016:2022 ,Tanzania_k13_Allele_frequency,col= 2,type="p",pch=20,cex=2)
-points(2025,0.4,col= 2,type="p",pch=20,cex=2)
-
-# save csv CI95
-incidence_ci95 <- data.frame(
-  Year = times_year,
-  Median = unlist( CI95[2,]),
-  `2.5%` = low,
-  `97.5%` = up
+plot(times_year, med,
+     type  = "l", lwd = 0,          # draw nothing yet; polygon goes first
+     log   = "y",                   # <-- LOG SCALE
+     xlab  = "Year",
+     ylab  = "Malaria Incidence",
+     ylim  = c(100, 3e7),
+     xlim  = c(2000, 2050),
+     panel.first = grid(col = "grey90", lty = 1))
+polygon(c(times_year, rev(times_year)),
+        c(low, rev(up)),
+        col    = "grey80",
+        border = NA)               # no border on polygon
+lines(times_year, med, lwd = 2, col = "blue")
+points(2015:2023, Tanzania_Incidence[, 2],
+       col = "black", pch = 21, bg = "white",
+       cex = 1.4, lwd = 1.5)
+legend("bottomleft",
+       legend = c("Confirmed Data", "Model Predicted Median"),
+       pch    = c(1, NA),
+       lty    = c(NA, 1),
+       lwd    = c(1.5, 2),
+       cex = 1,
+       col    = c("black", "blue")
 )
-write.csv(incidence_ci95, "incidence_ci95.csv", row.names = FALSE)
-resistance_ratio_ci95 <- data.frame(
-  Year = times_year,
-  Median = unlist( CI95_2[2,]),
-  `2.5%` = low2,
-  `97.5%` = up2
+title("Malaria Incidence")
+
+## --- Panel 2: Resistance Ratio ---
+CI95_2 <- data.frame(getCredibleIntervals(params.rand_remove_nan2, c(0.025, 0.5, 0.975)))
+up2  <- unlist(CI95_2[3,])
+low2 <- unlist(CI95_2[1,])
+med2 <- unlist(CI95_2[2,])
+
+plot(times_year, med2,
+     type = "l", lwd = 0,
+     xlab = "Year",
+     ylab = "Resistance Ratio",
+     ylim = c(0, 1),
+     xlim = c(2000, 2050),
+     panel.first = grid(col = "grey90", lty = 1))
+polygon(c(times_year, rev(times_year)),
+        c(low2, rev(up2)),
+        col    = "grey80",
+        border = NA)
+lines(times_year, med2, lwd = 2, col = "red") 
+points(2016:2022, Tanzania_k13_Allele_frequency,
+       col = "black", pch = 21, bg = "white",
+       cex = 1.4, lwd = 1.5)
+points(2025, 0.4,
+       col = "black", pch = 21, bg = "white",
+       cex = 1.4, lwd = 1.5)
+legend("topleft",
+       legend = c("Observed Data", "Model Predicted Median"),
+       pch    = c(1, NA),
+       lty    = c(NA, 1),
+       lwd    = c(1.5, 2),
+       col    = c("black", "red")
 )
-write.csv(resistance_ratio_ci95, "K13_resistance_ratio.csv", row.names = FALSE)
+title("Resistance Ratio")
 
-# If your sample has column names, this keeps them
-param_names <- c("Beta 1",	"Beta 2",	"Multiply Beta R",	
-                 "Resistant Fitness Cost",	"Phi For Beta Distribution")
-
-
-param_ci95_base <- t(apply(paramsample_test, 2, quantile, probs = c(0.025, 0.5, 0.975), na.rm = TRUE))
-param_ci95_base <- data.frame(
-  Parameter = param_names,
-  `2.5%` = param_ci95_base[,1],
-  Median = param_ci95_base[,2],
-  `97.5%`= param_ci95_base[,3],
-  row.names = NULL
-)
-
-param_ci95_base
-write.csv(param_ci95_base, "parameter_ci95.csv", row.names = FALSE)
