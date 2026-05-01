5ef7d6ea0a99ec2886e37efa60912aa782a63502
tanaphum <tana_jumbo@hotmail.com>
2026-05-01 09:43:00 +0700

    Reorganize plotting/results
    - Rename "3. plot result" to  "3. Results"
    - Clean up and reorganize result/plotting assets in 2 and 3

--- 3. Results/Figure 1 CI95/Plot_CI95.R
+++ 3. Results/Figure 1 CI95/Plot_CI95.R
@@ -3,20 +3,16 @@ library(deSolve)
 library(BayesianTools)
 library(scales)
 #Read model
-source("../model/model_D0_33_D1_67_baseline_change_birth_death_2betaSets_SS.R")
-source("init_parameter_optim_fitted.R")
+source("../../model/model_D0_33_D1_67_baseline_change_birth_death_2betaSets_SS.R")
+source("../init_parameter_MCMC.R")
 
-Tanzania_data <- read.csv("../data/Reported malaria cases by method of confirmation.csv")
+Tanzania_data <- read.csv("../../data/Reported malaria cases by method of confirmation.csv")
 Tanzania_Incidence <- Tanzania_data[6:14,c(1,4)]
-Tanzania_k13_Allele_frequency <- read.csv("../data/Tanzania_K13_Allele_frequency.csv")[,2]
+Tanzania_k13_Allele_frequency <- read.csv("../../data/Tanzania_K13_Allele_frequency.csv")[,2]
 
-init_state <- readRDS("init_state_2000.rds") # form 1.2 Find_init_state_equilibrium_incidence
+init_state <- readRDS("../init_state_2000.rds") # form 1.2 Find_init_state_equilibrium_incidence
 
 times <- seq(0,12*51)
-
-# Poputation
-totalpop_2000_2100 <- read.csv("../data/Tanzania_pop_2000_2100.csv", header = TRUE)
-
 
 MCMC_out <- readRDS("MCMC_out_example.rds")
 
